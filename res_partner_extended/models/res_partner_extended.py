bb5fb1ccba1c12a9e35f2153707c02d9fab7ae8e
Bassim Elsamaloty <99155821+Captivea-BEL@users.noreply.github.com>
2022-04-15 13:46:59 -0400

    Update res_partner_extended.py

--- res_partner_extended/models/res_partner_extended.py
+++ res_partner_extended/models/res_partner_extended.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, models, fields
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ResPartnerExtended(models.Model):
@@ -12,6 +16,7 @@ class ResPartnerExtended(models.Model):
         diff = dict(show_address=None, show_address_only=None, show_email=None, html_format=None, show_vat=None)
         names = dict(self.with_context(**diff).name_get())
         for partner in self:
+            partner.display_name = names.get(partner.id)  
             if partner.type == 'delivery':
                 if partner.parent_id.parent_id:
                     parent = ""
@@ -24,6 +29,29 @@ class ResPartnerExtended(models.Model):
                 else:
                     partner.display_name = partner.name    
             else:
-                partner.display_name = names.get(partner.id)  
+                partner.display_name = names.get(partner.id)           
 
-          
+    def name_get(self):
+        """ Return the categories' display name, including their direct
+            parent by default.
+            If ``context['partner_category_display']`` is ``'short'``, the short
+            version of the category name (without the direct parent) is used.
+            The default is the long version.
+        """
+        if self._context.get('default_type') == 'delivery':
+            res = []
+            for category in self:
+                res.append( (category.id, category.display_name) )
+            return res  
+        if self._context.get('partner_category_display') == 'short':
+            return super(PartnerCategory, self).name_get()
+
+        res = []
+        for category in self:
+            names = []
+            current = category
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append( (category.id, ' / '.join(reversed(names))) )
+        return res  
